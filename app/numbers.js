bd65253112e5c4dba7a0907260d5c08c7b0e08f4
James <jamesowenpabon@gmail.com>
2016-05-20 19:53:30 -0400

    Numbers Completed

--- app/numbers.js
+++ app/numbers.js
@@ -6,14 +6,27 @@ exports.numbersAnswers = {
   },
 
   base10: function(str) {
+    var digit = parseInt(str, 2);
+    return(digit);
 
   },
 
   convertToBinary: function(num) {
-
+    var bi = num.toString(2);
+    var biLen = bi.length;
+    var pad = (biLen % 2 === 0) ? false : true;
+    if (pad) {bi = '0'.concat(bi)}
+    return(bi);
   },
 
   multiply: function(a, b) {
-
+    var prd = a*b;
+    var sa = a.toString();
+    var sb = b.toString();
+    var sal = (sa.length) - (sa.indexOf("."),1);
+    var sbl = (sb.length) - (sb.indexOf("."),1);
+    var dec = sal > sbl ? sal : sbl;
+    prd = prd.toFixed(dec)
+    return parseFloat(prd);
   }
 };
