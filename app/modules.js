2be2aaf4b9a3aad49e44eaf79ca3e906157fbe68
James <jamesowenpabon@gmail.com>
2016-05-20 17:38:54 -0400

    Modules Completed

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    this.greeting = str1;
+    this.name = str2;
+    function sayIt()  {
+      console.log(this);
+      return this.greeting + ", " + this.name;
+    }
+    return {
+      sayIt : sayIt,
+      name : this.name,
+      greeting : this.greeting
+    }
   }
 };
