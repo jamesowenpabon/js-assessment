31fa105f9b6334c16e494722145f673a6ae0084d
James <jamesowenpabon@gmail.com>
2016-05-20 14:29:38 -0400

    App Flow Completed

--- app/flowControl.js
+++ app/flowControl.js
@@ -10,6 +10,21 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
-
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log('fizzbuzz')
+      return 'fizzbuzz';
+    } else if (num % 3 === 0) {
+      console.log('fizz')
+      return 'fizz'
+    } else if (num % 5 === 0) {
+      console.log('buzz')
+      return 'buzz'
+    } else if (num % 1 === 0) {
+      console.log(num)
+      return num;
+    } else {
+      console.log("false");
+      return false;
+    }
   }
 };
