608034cb4cf73c7d7990685a70e515f3ab55d731
James <jamesowenpabon@gmail.com>
2016-05-20 20:09:46 -0400

    Count Completed

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var intCounter = parseInt(start, 10);
+    var timer = setInterval(function(){
+      if (intCounter > end) {
+        clearInterval(timer);
+      } else {
+        console.log(intCounter);
+        intCounter ++
+      }
+    },100,end);
 
+    var stop = {
+      cancel : function(){
+        clearInterval(timer);
+      }
+    }
+    return stop;
   }
 };
